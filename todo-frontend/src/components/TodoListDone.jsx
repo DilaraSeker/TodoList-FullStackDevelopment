8be4a1432ac75bfca4c037aaff9868d281c78235
DilaraSeker <dlrseker@gmail.com>
2023-11-15 23:50:36 +0300

    Update React Bootstrap

--- todo-frontend/src/components/TodoListDone.jsx
+++ todo-frontend/src/components/TodoListDone.jsx
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
+// API SERVICE
+import TodoApi from "../service/TodoApi";
+// CSS
+import '../style.css';
+// TODO ITEM FOR EACH TODO
+import TodoItem from './TodoItem';
 
+// DONE TODOS COMPONENT
 export default class TodoListDone extends Component {
+  // CONSTRUCTOR
+  constructor(props) {
+    super(props);
+     // STATES
+     this.state = {
+      todoList: [],
+      show: false,
+    }
+  }
+
+  // BEFORE RENDER COMPONENT
+  componentDidMount() {
+    TodoApi.todoApiList().then(
+      (response) => {
+       // if(response.data.completed == true){
+          this.setState({
+            todoList: response.data
+          })
+        //}
+        
+      }).catch((error) => {
+        console.log("Cannot get list from database")
+      });
+      
+  }
+
   render() {
+    
     return (
-      <div>TodoListDone</div>
+      <div className="list">
+        {
+          this.state.todoList.map(temp =>
+            
+           
+            <TodoItem todo={temp}>{console.log(temp)}</TodoItem>
+        
+          )
+        }
+      </div>
     )
   }
 }
