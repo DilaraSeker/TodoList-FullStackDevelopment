9b3460b3c986727bacdf23753f31f4bdce5ccf85
DilaraSeker <dlrseker@gmail.com>
2023-11-15 21:01:39 +0300

    Create Spring Boot project and packages

---
+++ src/main/java/com/dilaraseker/todolist/TodoListFullStackDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.dilaraseker.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListFullStackDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListFullStackDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dilaraseker/todolist/TodoListFullStackDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilaraseker.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListFullStackDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
