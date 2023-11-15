722fcf7a697d99a60fca936579d0a59d91e5306b
DilaraSeker <dlrseker@gmail.com>
2023-11-15 22:34:24 +0300

    Add List Buttons component and functionality

--- todo-frontend/src/components/TodoListButtons.jsx
+++ todo-frontend/src/components/TodoListButtons.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
+// ROUTER
+import { withRouter } from "react-router-dom";
 
-export default class TodoListButtons extends Component {
+// LIST BUTTONS COMPONENT
+class TodoListButtons extends Component {
+  // CONSTRUCTOR
+  constructor(props) {
+    super(props);
+    //BIND
+    this.listAllTodos = this.listAllTodos.bind(this);
+    this.listDoneTodos = this.listDoneTodos.bind(this);
+    this.listUndoneTodos = this.listUndoneTodos.bind(this);
+  }
+  // ALL
+  listAllTodos() {
+    this.props.history.push("/todos/list")
+  }
+  // DONE
+  listDoneTodos() {
+    this.props.history.push("/todos/done")
+  }
+  // TODO (UNDONE)
+  listUndoneTodos() {
+    this.props.history.push("/todos/undone")
+  }
   render() {
     return (
-      <div>TodoListButtons</div>
+      <div className="btns-row">
+        <button className="list-btns" onClick={this.listAllTodos} type="submit" name="submit" id="all" title="All">
+          All
+        </button>
+
+        <button className="list-btns" onClick={this.listDoneTodos} type="submit" name="submit" id="done" title="Done">
+          Done
+        </button>
+
+        <button className="list-btns" onClick={this.listUndoneTodos} type="submit" name="submit" id="todo" title="Todo">
+          Todo
+        </button>
+      </div >
     )
   }
-}
+} // END COMPONENT
+export default withRouter(TodoListButtons);
