7a39b64413f979afdeca4ecc54a2dc9ccfeea5c6
DilaraSeker <dlrseker@gmail.com>
2023-11-15 22:39:43 +0300

    Create list all todos component

--- todo-frontend/src/components/TodoListAll.jsx
+++ todo-frontend/src/components/TodoListAll.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+// API SERVICE
+import TodoApi from "../services/TodoApi";
+// CSS
+import '../style.css';
+// TODO ITEM FOR EACH TODO
+import TodoItem from './TodoItem';
 
+// ALL TODOS COMPONENT
 export default class TodoListAll extends Component {
+  // CONSTRUCTOR
+  constructor(props) {
+    super(props);
+
+  }
+
+  // BEFORE RENDER COMPONENT
+  componentDidMount() {
+    TodoApi.todoApiList().then(
+      (response) => {
+        this.setState({
+          todoList: response.data
+        })
+      }).catch((error) => {
+        console.log("Cannot get list from database")
+      });
+
+  }
+
   render() {
     return (
-      <div>TodoListAll</div>
+      <div className="list">
+        {
+          this.state.todoList.map(temp =>
+            <TodoItem todo={temp}>{console.log(temp)}</TodoItem>
+          )
+        }
+      </div>
     )
   }
 }
