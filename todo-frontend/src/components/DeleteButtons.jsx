af3143af1e3f7a7f5604b524d82df9cfd5ca025f
DilaraSeker <dlrseker@gmail.com>
2023-11-16 08:00:12 +0300

    Delete done tasks

--- todo-frontend/src/components/DeleteButtons.jsx
+++ todo-frontend/src/components/DeleteButtons.jsx
@@ -6,14 +6,31 @@ import TodoApi from "../service/TodoApi";
 
 // LIST BUTTONS COMPONENT
 class DeleteButtons extends Component {
+
     // CONSTRUCTOR
     constructor(props) {
         super(props);
+        // STATES
+        this.state = {
+            todoList: [],
+        }
         //BIND
         this.deleteAllTodos = this.deleteAllTodos.bind(this);
         this.deleteDoneTodos = this.deleteDoneTodos.bind(this);
-
     }
+
+    // BEFORE RENDER COMPONENT
+    componentDidMount() {
+        TodoApi.todoApiList().then(
+            (response) => {
+                this.setState({
+                    todoList: response.data
+                })
+            }).catch((error) => {
+                console.log("Cannot get list from database")
+            });
+    }
+
     // DELETE ALL
     deleteAllTodos() {
         TodoApi.todoApiDeleteAll().then(
@@ -24,9 +41,30 @@ class DeleteButtons extends Component {
             });
         window.location.reload()
     }
+
     // DELETE DONE
     deleteDoneTodos() {
-
+        if (this.state.todoList.completed === false) {
+            console.log("false")
+        }
+        const filteredArray = this.state.todoList.filter((item) => item.completed === true);
+        console.log(filteredArray)
+        console.log("selam")
+        filteredArray.map(temp => {
+            TodoApi.todoApiDelete(temp.id).then(
+                (response) => {
+                    this.setState({
+                        todoList: this.state.todoList.filter(
+                            todoList => todoList.id !== temp.id
+                        )
+                    });
+                    console.log()
+                }).catch(() => {
+                    console.log("Delete task not work")
+                });
+        }
+        )
+        window.location.reload()
     }
 
     render() {
