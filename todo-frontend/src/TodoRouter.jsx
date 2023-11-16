48daf00eb149b206015d5dbe0916997b85c24bff
DilaraSeker <dlrseker@gmail.com>
2023-11-16 04:46:09 +0300

    Add delete buttons component to router

--- todo-frontend/src/TodoRouter.jsx
+++ todo-frontend/src/TodoRouter.jsx
@@ -2,27 +2,22 @@
 import React from 'react'
 // CSS
 import './style.css';
-
 // ROUTER
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 // INPUT
 import TodoInput from './components/TodoInput';
-
 // LIST BUTTONS
 import TodoListButtons from './components/TodoListButtons';
-
 // LIST ALL TODOS
 import TodoListAll from './components/TodoListAll';
-
 // LIST DONE TODOS
 import TodoListDone from './components/TodoListDone';
-
 // LIST UNDONE TODOS
 import TodoListUndone from './components/TodoListUndone';
-
 // UPDATE
 import TodoUpdate from './components/TodoUpdate';
+// DELETE BUTTONS
+import DeleteButtons from './components/DeleteButtons';
 
 // TODO ROUTER FUNCTION COMPONENT
 function TodoRouter() {
@@ -36,7 +31,7 @@ function TodoRouter() {
                 <div className="list-container">
                     <h2>TodoList</h2>
                     <TodoListButtons />
-                    <br /><br />
+                    <br />
                     <Switch>
                         <Route path="/" exact component={TodoListAll}></Route>
                         <Route path="/todos/list" component={TodoListAll}></Route>
@@ -44,9 +39,10 @@ function TodoRouter() {
                         <Route path="/todos/undone" component={TodoListUndone}></Route>
                         <Route path="/todo/update/:id" component={TodoUpdate}></Route>
                     </Switch>
+                    <br />
+                    <DeleteButtons />
                 </div>
             </Router>
-
         </div>
     ) //end return
 } // end function 
