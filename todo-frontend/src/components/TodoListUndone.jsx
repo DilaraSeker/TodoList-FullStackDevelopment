b3a9ce68db857dde79e533b3c87cfdb513eef143
DilaraSeker <dlrseker@gmail.com>
2023-11-16 05:30:08 +0300

    Create Undone Component

--- todo-frontend/src/components/TodoListUndone.jsx
+++ todo-frontend/src/components/TodoListUndone.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react'
+// API SERVICE
+import TodoApi from "../service/TodoApi";
+// CSS
+import '../style.css';
+// TODO ITEM FOR EACH TODO
+import TodoItem from './TodoItem';
 
+// DONE TODOS COMPONENT
 export default class TodoListUndone extends Component {
+  // CONSTRUCTOR
+  constructor(props) {
+    super(props);
+    // STATES
+    this.state = {
+      todoList: [],
+      show: false,
+      todoListDone: [],
+    }
+  }
+
+  // BEFORE RENDER COMPONENT
+  componentDidMount() {
+    TodoApi.todoApiList().then(
+      (response) => {
+        // if(response.data.completed == true){
+        this.setState({
+          todoList: response.data
+        })
+        //}
+
+      }).catch((error) => {
+        console.log("Cannot get list from database")
+      });
+  }
+  
+  // Filter condition
+  filterCondition = (item) => item.completed === false;
+
   render() {
+    const filteredArray = this.state.todoList.filter(this.filterCondition);
     return (
-      <div>TodoListUndone</div>
+      <div className="list">
+        {
+          filteredArray.map(temp =>
+            <div>
+              <TodoItem todo={temp}>{console.log(temp)}</TodoItem>
+              <div className="space"></div>
+            </div>
+          )
+        }
+      </div>
     )
   }
 }
